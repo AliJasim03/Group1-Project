379c9a161f3b3b0e539349c328aa3c4367d58f88
Kameleon21 <kamilrogozinski29@gmail.com>
2024-06-22 19:13:08 +0100

    Added Spring Boot setup to the project

---
+++ Group1-BankingApp/src/main/java/com/example/group1bankingapp/Group1BankingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.group1bankingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group1BankingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Group1BankingAppApplication.class, args);
+    }
+
+}
---
+++ Group1-BankingApp/src/test/java/com/example/group1bankingapp/Group1BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.group1bankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group1BankingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
